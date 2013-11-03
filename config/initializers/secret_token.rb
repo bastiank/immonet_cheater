bf8a9c6ddd441ca3025879f17dfb99b1350dc2a9
Bastian Kriesten <bastian.kriesten@jimdo.com>
2013-11-03 18:43:09 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImmonetCheater::Application.config.secret_key_base = '38d640f9c6738022e2fefcdd276b9a3e053cf2d71652c43a741bcf68068982d9e78dbef690689d4d47761540d43f6777d62d4e76391848374ce5ffa254428a75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImmonetCheater::Application.config.session_store :cookie_store, key: '_immonet_cheater_session'
